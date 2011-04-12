ab8d4892e10b56218b5b398be6ce875a948f69d1
Michael Berkowitz <michael.berkowitz@gmail.com>
2011-04-12 00:37:18 -0400

    Adds basic vertex and edge functionality.

---
+++ lib/organdiarum.rb
@@ -0,0 +1,86 @@
+module Organdiarum
+  class Base
+    include Comparable
+
+    attr_reader :name
+    def initialize(name=nil)
+      @name = name || generate_uuid
+    end
+
+    def <=>(other)
+      self.name <=> other.name
+    end
+
+    private
+
+    def generate_uuid
+      `uuidgen`.strip.gsub(/-/, "").gsub(/\w/) {|m| rand(10) < 5 ? m : m.downcase}
+    end
+  end
+
+  class Digraph < Base
+    attr_reader :vertices, :edges
+    def initialize(name=nil)
+      super(name)
+      @vertices = []
+      @edges = []
+    end
+
+    def add_vertices(*vertices)
+      vertices.each do |vertex|
+        vertex = Vertex.new(vertex) unless vertex.instance_of?(Vertex)
+        @vertices << vertex unless @vertices.include?(vertex)
+      end
+      self
+    end
+    alias :add_vertex :add_vertices
+    alias :av :add_vertices
+    alias :avs :add_vertices
+
+    def add_edge(edge_or_from, to=nil)
+      if edge_or_from.instance_of?(Edge)
+        @edges << edge_or_from
+      else
+        @edges << Edge.new(edge_or_from, to)
+      end
+      self
+    end
+    alias :ae :add_edge
+
+    def add_edges(*edges)
+      edges.each do |edge|
+        add_edge(*Array(edge))
+      end
+      self
+    end
+    alias :aes :add_edges
+
+    def to_s
+      ["VERTICES",
+       @vertices.map{|vertex| "  #{vertex.to_s}"},
+       "EDGES",
+       @edges.map{|edge| " #{edge.to_s}"},
+      ].flatten.join("\n")
+    end
+  end
+
+  class Vertex < Base
+    def initialize(name=nil)
+      super(name)
+    end
+
+    def to_s
+      @name
+    end
+  end
+
+  class Edge < Base
+    def initialize(from, to)
+      @from, @to = from, to
+    end
+
+    def to_s
+      "#{@from} --> #{@to}"
+    end
+  end
+end
--- spec/organdiarum_spec.rb
+++ spec/organdiarum_spec.rb
@@ -1,7 +1,60 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Organdiarum" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "name" do
+    it "should be set correctly when a name is provided" do
+      Organdiarum::Base.new("my new base object").name.should == "my new base object"
+    end
+
+    it "should be set to a random uuid when no name is provided" do
+      Organdiarum::Base.new.name.size.should == 32
+    end
+  end
+
+  describe "Digraph" do
+    before { @d = Organdiarum::Digraph.new }
+
+    it "should be able to add a vertex object" do
+      vertex = Organdiarum::Vertex.new("city")
+      @d.add_vertex(vertex)
+      @d.vertices.should == [vertex]
+    end
+
+    it "should be able to add a vertex from a string" do
+      @d.add_vertex("set")
+      @d.vertices.should == [Organdiarum::Vertex.new("set")]
+    end
+
+    it "should silently not add duplicate vertices" do
+      @d.add_vertex("a")
+      @d.add_vertices("b", "c", "a", Organdiarum::Vertex.new("A"), "city")
+
+      @d.vertices.map(&:name).should == %w{a b c A city}
+    end
+
+    it "should add an edge from a pair of strings" do
+      @d.add_vertices("a", "b")
+      @d.add_edge("a", "b")
+
+      @d.edges.map(&:to_s).should == ["a --> b"]
+    end
+
+    it "should add an edge object" do
+      @d.add_vertices("a", "b")
+      edge = Organdiarum::Edge.new("a", "b")
+      @d.add_edge(edge)
+
+      @d.edges.map(&:to_s).should == ["a --> b"]
+    end
+
+    it "should add multiple edges" do
+      @d.add_vertices(*%w{a b c d})
+      edge = Organdiarum::Edge.new("a", "b")
+
+      @d.add_edges(edge, ["b", "c"], Organdiarum::Edge.new("d", "a"))
+
+      @d.edges.map(&:to_s).should ==
+        ["a --> b", "b --> c", "d --> a"]
+    end
   end
 end
